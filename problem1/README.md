a0f79e681bc535bce9b2c8e79beaa25138617841
Andrew Latham <acl68@case.edu>
2012-07-05 20:30:05 -0400

    Fixed file structure

---
+++ problem1/README.md
@@ -0,0 +1,7 @@
+Project-Euler-Problems
+======================
+
+A collection of my solutions to Project Euler Problems
+
+======================
+Problem 1:  7/5/2012
---
+++ problem1/three_or_five.rb
@@ -0,0 +1,8 @@
+# Add all the natural numbers below one thousand that are multiples of 3 or 5. #
+x = 1
+total = 0
+while x < 1000 do
+  if ((x % 3 == 0) || (x % 5 == 0)) then total += x end
+  x += 1
+end
+puts total
